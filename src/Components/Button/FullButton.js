fdc6c676833f13f9fc1c60a3704ed7463250e078
김민선 <alstjs4739@kookmin.ac.kr>
2023-05-14 15:22:07 +0900

    chore: 버튼 컴포넌트 수정

--- src/Components/Button/FullButton.js
+++ src/Components/Button/FullButton.js
@@ -3,7 +3,16 @@ import * as style from "./styles";
 
 function FullButton(props) {
   return (
-    <style.FullButton onClick={props.onClick}>
+    <style.FullButton
+      onClick={props.onClick}
+      width={props.width}
+      height={props.height}
+      border={props.border}
+      backgroundColor={props.backgroundColor}
+      color={props.color}
+      fontSize={props.fontSize}
+      btnSubName={props.btnSubName}
+    >
       {props.src && <img src={props.src} />}
       {props.btnName && <span>{props.btnName}</span>}
       {props.btnSubName && <span>{props.btnSubName}</span>}
