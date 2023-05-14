dff7b44994e13b30f23c2ae9850d5b8aa8956117
didskfo <didskfo2003@gmail.com>
2023-05-14 14:22:13 +0900

    feat. 컴포넌트로 수정

--- src/Pages/PasswordChange/PasswordChange.js
+++ src/Pages/PasswordChange/PasswordChange.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import * as style from "./styles";
+import Input from "../../Components/Input/Input";
 
 function PasswordChange() {
   return (
@@ -13,30 +14,53 @@ function PasswordChange() {
           <style.Set>완료</style.Set>
         </style.Head>
       </header>
-      <style.Label>현재 비밀번호</style.Label>
+      {/* <style.Label>현재 비밀번호</style.Label>
       <style.PresentPasswordInput>
         <input
           type="text"
           style={{ border: 0, outline: 0, width: "413px" }}
           placeholder="현재 비밀번호를 입력해주세요"
         />
-      </style.PresentPasswordInput>
-      <style.Label>새 비밀번호</style.Label>
+      </style.PresentPasswordInput> */}
+      <Input
+        titlemarginBottom={"10px"}
+        title={"현재 비밀번호"}
+        marginBottom={"30px"}
+        name={"PasswordChangePresentPasswordInput"}
+        type={"password"}
+        placeholder={"현재 비밀번호를 입력해주세요"}
+      />
+      {/* <style.Label>새 비밀번호</style.Label>
       <style.NewPasswordInput>
         <input
           type="password"
           style={{ border: 0, outline: 0, width: "413px" }}
           placeholder="새 비밀번호를 입력해주세요"
         />
-      </style.NewPasswordInput>
-      <style.Label>새 비밀번호 확인</style.Label>
+      </style.NewPasswordInput> */}
+      <Input
+        titlemarginBottom={"10px"}
+        title={"새 비밀번호"}
+        marginBottom={"30px"}
+        name={"PasswordChangeNewInput"}
+        placeholder={"새 비밀번호를 입력해주세요"}
+      />
+      {/* <style.Label>새 비밀번호 확인</style.Label>
       <style.NewPasswordCheckInput>
         <input
           type="password"
           style={{ border: 0, outline: 0, width: "413px" }}
           placeholder="새 비밀번호를 한 번 더 입력해주세요"
         />
-      </style.NewPasswordCheckInput>
+      </style.NewPasswordCheckInput> */}
+      <Input
+        titlemarginBottom={"10px"}
+        title={"새 비밀번호 확인"}
+        marginBottom={"30px"}
+        name={"PasswordChagneCheckInput"}
+        type={"password"}
+        placeholder={"새 비밀번호를 한 번 더 입력해주세요"}
+      />
     </div>
   );
 }
