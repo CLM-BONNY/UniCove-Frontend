5058bb83a59e675ec40a68507a4a2e33a2279d0a
didskfo <didskfo2003@gmail.com>
2023-05-15 19:03:26 +0900

    feat. Service 페이지 레이아웃

---
+++ src/Pages/Service/Service.js
@@ -0,0 +1,13 @@
+import React from "react";
+import * as style from "./styles";
+import Header from "../../Components/Header/Header";
+
+function Service() {
+  return (
+    <style.Wrap>
+      <Header title={"서비스 이용약관"} />
+    </style.Wrap>
+  );
+}
+
+export default Service;
---
+++ src/Pages/Service/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+  padding: 0px 20px 0;
+`;
