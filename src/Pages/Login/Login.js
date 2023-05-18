d96ab9f10632162bb914e40249412d61a2458015
didskfo <didskfo2003@gmail.com>
2023-05-19 00:43:07 +0900

    feat. 로고 수정 및 주석 삭제

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -11,20 +11,10 @@ function Login() {
         <img
           className="logoImg"
           alt="로고 이미지"
-          src={process.env.PUBLIC_URL + "Images/logoImg.svg"}
+          src={process.env.PUBLIC_URL + "Images/Login/UniCove.svg"}
         ></img>
       </style.LogoBlock>
       <form>
-        {/* <style.Label>아이디</style.Label>
-        <style.InputBlock>
-          <div>
-            <input
-              type="text"
-              style={{ border: 0, outline: 0 }}
-              placeholder="아이디를 입력해주세요"
-            />
-          </div>
-        </style.InputBlock> */}
         <Input
           titlemarginBottom={"10px"}
           title={"아이디"}
@@ -32,16 +22,6 @@ function Login() {
           name={"LoginIdInput"}
           placeholder={"아이디를 입력해주세요"}
         />
-        {/* <style.Label>비밀번호</style.Label>
-        <style.InputBlock>
-          <div>
-            <input
-              type="password"
-              style={{ border: 0, outline: 0 }}
-              placeholder="비밀번호를 입력해주세요"
-            />
-          </div>
-        </style.InputBlock> */}
         <Input
           titlemarginBottom={"10px"}
           title={"비밀번호"}
@@ -51,10 +31,22 @@ function Login() {
           placeholder={"비밀번호를 입력해주세요"}
         />
       </form>
-      {/* <style.LoginButtonBlock type="submit">로그인</style.LoginButtonBlock> */}
-      <FullButton btnName={"로그인"} />
-      <StrokeButton btnName={"회원가입"} />
-      {/* <style.ButtonBlock type="button">회원가입</style.ButtonBlock> */}
+      <style.OuterButton>
+        <FullButton
+          btnName={"로그인"}
+          width={"400px"}
+          height={"25px"}
+          margin={"10px 0px"}
+        />
+      </style.OuterButton>
+      <style.OuterButton>
+        <StrokeButton
+          btnName={"회원가입"}
+          width={"400px"}
+          height={"25px"}
+          margin={"0px 0px 20px 0px"}
+        />
+      </style.OuterButton>
     </style.Wrap>
   );
 }
