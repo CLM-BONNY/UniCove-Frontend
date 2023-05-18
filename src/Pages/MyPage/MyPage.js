a9ccb2336547d38f7a2227795fd3cf489a7dedd6
didskfo <didskfo2003@gmail.com>
2023-05-19 00:44:47 +0900

    chore. 코드 간결화

--- src/Pages/MyPage/MyPage.js
+++ src/Pages/MyPage/MyPage.js
@@ -5,18 +5,10 @@ import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 
 function MyPage() {
+  const title = "내 정보";
   return (
     <style.Wrap>
-      {/* <header>
-        <style.Head>
-          <style.Back
-            src={process.env.PUBLIC_URL + "Images/Join/back.svg"}
-          ></style.Back>
-          <style.Title>내 정보</style.Title>
-          <style.Set>수정</style.Set>
-        </style.Head>
-      </header> */}
-      <Header title={"내 정보"} />
+      <Header title={title} />
       <style.ProfileImg>
         <img
           src={process.env.PUBLIC_URL + "Images/Mypage/basicProfileImg.svg"}
@@ -30,8 +22,6 @@ function MyPage() {
         placeholder={"홍길동"}
         color={"black"}
       />
-      {/* <style.Label>전화번호</style.Label>
-      <style.Phone>010-1234-5678</style.Phone> */}
       <ReadonlyInput
         titlemarginBottom={"10px"}
         title={"전화번호"}
@@ -40,8 +30,6 @@ function MyPage() {
         placeholder={"010-1234-5678"}
         color={"black"}
       />
-      {/* <style.Label>주소</style.Label>
-      <style.Place>성북구</style.Place> */}
       <ReadonlyInput
         titlemarginBottom={"10px"}
         title={"주소"}
@@ -50,8 +38,6 @@ function MyPage() {
         placeholder={"성북구"}
         color={"black"}
       />
-      {/* <style.Label>아이디</style.Label>
-      <style.Id>gildong12</style.Id> */}
       <ReadonlyInput
         titlemarginBottom={"10px"}
         title={"아이디"}
@@ -60,7 +46,7 @@ function MyPage() {
         placeholder={"gildong12"}
         color={"black"}
       />
-      <Footer title={"내 정보"} />
+      <Footer title={title} />
     </style.Wrap>
   );
 }
--- src/Pages/MyPageEdit/MyPageEdit.js
+++ src/Pages/MyPageEdit/MyPageEdit.js
@@ -3,71 +3,141 @@ import * as style from "./styles";
 import Input from "../../Components/Input/Input";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
+import FullButton from "../../Components/Button/FullButton";
 
 function MyPageEdit() {
+  const title = "내 정보 수정";
+  const [namecolor, setNameColor] = useState(true);
+  const handleNameColor = () => {
+    setNameColor(!namecolor);
+  };
+
+  const [phonecolor, setPhoneColor] = useState(true);
+  const handlePhoneColor = () => {
+    setPhoneColor(!phonecolor);
+  };
+
+  const [placecolor, setPlaceColor] = useState(true);
+  const handlePlaceColor = () => {
+    setPlaceColor(!placecolor);
+  };
+
+  const [idcolor, setIdColor] = useState(true);
+  const handleIdColor = () => {
+    setIdColor(!idcolor);
+  };
+
+  const [nameplaceholder, setNameplaceholder] = useState("홍길동");
+  const handleNameClick = () => {
+    setNameplaceholder("변경할 이름을 입력해주세요");
+    setPhoneColor("black");
+    setPlaceColor("black");
+    setIdColor("black");
+    setPhoneplaceholder("010-1234-5678");
+    setPlaceplaceholder("성북구");
+    setIdplaceholder("gildong12");
+  };
+
+  const [phoneplaceholder, setPhoneplaceholder] = useState("010-1234-5678");
+  const handlePhoneClick = () => {
+    setPhoneplaceholder("변경할 전화번호를 입력해주세요");
+    setNameColor("black");
+    setPlaceColor("black");
+    setIdColor("black");
+    setNameplaceholder("홍길동");
+    setPlaceplaceholder("성북구");
+    setIdplaceholder("gildong12");
+  };
+
+  const [placeplaceholder, setPlaceplaceholder] = useState("성북구");
+  const handlePlaceClick = () => {
+    setPlaceplaceholder("변경할 주소를 입력해주세요");
+    setNameColor("black");
+    setPhoneColor("black");
+    setIdColor("black");
+    setNameplaceholder("홍길동");
+    setPhoneplaceholder("010-1234-5678");
+    setIdplaceholder("gildong12");
+  };
+
+  const [idplaceholder, setIdplaceholder] = useState("gildong12");
+  const handleIdClick = () => {
+    setIdplaceholder("변경할 아이디를 입력해주세요");
+    setNameColor("black");
+    setPhoneColor("black");
+    setPlaceColor("black");
+    setNameplaceholder("홍길동");
+    setPhoneplaceholder("010-1234-5678");
+    setPlaceplaceholder("성북구");
+  };
+
   return (
     <style.Wrap>
-      {/* <header>
-        <style.Head>
-          <style.Back
-            src={process.env.PUBLIC_URL + "Images/Join/back.svg"}
-          ></style.Back>
-          <style.Title>내 정보</style.Title>
-          <style.Set>완료</style.Set>
-        </style.Head>
-      </header> */}
-      <Header title={"내 정보 수정"} />
+      <Header title={title} />
       <style.ProfileImg>
         <img
           src={process.env.PUBLIC_URL + "Images/Mypage/basicProfileImg.svg"}
         ></img>
       </style.ProfileImg>
-      <style.Set>
-        <img
-          src={process.env.PUBLIC_URL + "Images/MyPageEdit/Camera.svg"}
-        ></img>
-      </style.Set>
-      {/* <style.Label>이름</style.Label>
-      <style.Name>홍길동</style.Name> */}
+      <FullButton
+        backgroundColor={"#0F4C75"}
+        position={"absolute"}
+        width={"51px"}
+        height={"22.35px"}
+        top={"243px"}
+        left={"252px"}
+        border={"30px"}
+        src={process.env.PUBLIC_URL + "Images/MyPageEdit/Camera.svg"}
+      ></FullButton>
       <Input
         titlemarginBottom={"10px"}
         title={"이름"}
         marginBottom={"30px"}
         name={"MypageEditName"}
-        placeholder={"홍길동"}
-        color={"black"}
+        placeholder={nameplaceholder}
+        color={namecolor ? "black" : "gray"}
+        onClick={() => {
+          handleNameColor();
+          handleNameClick();
+        }}
       />
-      {/* <style.Label>전화번호</style.Label>
-      <style.Phone>010-1234-5678</style.Phone> */}
       <Input
         titlemarginBottom={"10px"}
         title={"전화번호"}
         marginBottom={"30px"}
         name={"MypageEditPhone"}
-        placeholder={"010-1234-5678"}
-        color={"black"}
+        placeholder={phoneplaceholder}
+        color={phonecolor ? "black" : "gray"}
+        onClick={() => {
+          handlePhoneColor();
+          handlePhoneClick();
+        }}
       />
-      {/* <style.Label>주소</style.Label>
-      <style.Place>성북구</style.Place> */}
       <Input
         titlemarginBottom={"10px"}
         title={"주소"}
         marginBottom={"30px"}
         name={"MypageEditPlace"}
-        placeholder={"성북구"}
-        color={"black"}
+        placeholder={placeplaceholder}
+        color={placecolor ? "black" : "gray"}
+        onClick={() => {
+          handlePlaceColor();
+          handlePlaceClick();
+        }}
       />
-      {/* <style.Label>아이디</style.Label>
-      <style.Id>gildong12</style.Id> */}
       <Input
         titlemarginBottom={"10px"}
         title={"아이디"}
         marginBottom={"30px"}
         name={"MypageEditId"}
-        placeholder={"gildong12"}
-        color={"black"}
+        placeholder={idplaceholder}
+        color={idcolor ? "black" : "gray"}
+        onClick={() => {
+          handleIdColor();
+          handleIdClick();
+        }}
       />
-      <Footer title={"내 정보 수정"} />
+      <Footer title={title} />
     </style.Wrap>
   );
 }
