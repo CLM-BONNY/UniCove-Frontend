232a54427f1ad7922a61e2eceefacfde904fc9b3
didskfo <didskfo2003@gmail.com>
2023-05-20 00:46:53 +0900

    feat. 회원가입 페이지 조건 넣기

--- src/Pages/Join/Join.js
+++ src/Pages/Join/Join.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import * as style from "./styles";
 import Input from "../../Components/Input/Input";
 import FullButton from "../../Components/Button/FullButton";
@@ -7,6 +7,71 @@ import Header from "../../Components/Header/Header";
 
 function Join() {
   const title = "회원가입";
+  const [write, setWrite] = useState({
+    JoinNameInput: "",
+    JoinPhoneInput: "",
+    JoinPlaceInput: "",
+    JoinIdInput: "",
+    JoinPasswordInput: "",
+    JoinPasswordCheckInput: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setWrite((prevWrite) => ({
+      ...prevWrite,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = async () => {
+    console.log(write);
+    const inputName = write.JoinNameInput;
+    const inputPhone = write.JoinPhoneInput;
+    const inputPlace = write.JoinPlaceInput;
+    const inputId = write.JoinIdInput;
+    const inputPassword = write.JoinPasswordInput;
+    const inputPasswordCheck = write.JoinPasswordCheckInput;
+    const phoneRegex = /^\d{3}-\d{3,4}-\d{4}$/;
+    const filteredId = inputId.replace(/[^a-zA-Z0-9]/g, "");
+    const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
+    if (
+      inputName.length === 0 ||
+      inputPhone.length === 0 ||
+      inputPlace === "text" ||
+      inputId.length === 0 ||
+      inputPassword === 0 ||
+      inputPasswordCheck === 0
+    ) {
+      alert("전부 입력해주세요");
+    }
+    if (!phoneRegex.test(inputPhone)) {
+      alert("알맞지 않은 전화번호입니다");
+    }
+    if (!(filteredId === inputId)) {
+      alert("알맞지 않은 아이디입니다");
+    }
+    if (inputPassword.length < 8 || inputPassword.length > 10) {
+      alert("비밀번호를 8-10글자 이내로 입력해주세요");
+    }
+    if (!passwordRegex.test(inputPassword)) {
+      alert("알맞지 않은 비밀번호입니다");
+    }
+    if (!(inputPassword === inputPasswordCheck)) {
+      alert("비밀번호를 동일하게 입력해주세요");
+    }
+  };
+  const inputRef = useRef(null);
+  const onUploadImage = useCallback((e) => {
+    if (!e.target.files) {
+      return;
+    }
+    console.log(e.target.files[0].name);
+  }, []);
+  const onUploadImageButtonClick = useCallback(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.click();
+  }, []);
   const [selectColor, setSelectColor] = useState("gray");
   const handleSelectColor = (e) => {
     if (e.target.value === "text") {
@@ -14,7 +79,11 @@ function Join() {
     } else {
       setSelectColor("black");
     }
-    console.log(e.target.value);
+    const { name, value } = e.target;
+    setWrite((prevWrite) => ({
+      ...prevWrite,
+      [name]: value,
+    }));
   };
 
   return (
@@ -33,24 +102,38 @@ function Join() {
         top={"243px"}
         left={"252px"}
         border={"30px"}
+        onClick={onUploadImageButtonClick}
       ></FullButton>
+      <input
+        type="file"
+        accept="image/*"
+        style={{ display: "none" }}
+        ref={inputRef}
+        onChange={onUploadImage}
+      />
       <Input
         titlemarginBottom={"10px"}
         title={"이름"}
         marginBottom={"30px"}
         name={"JoinNameInput"}
         placeholder={"이름을 입력해주세요"}
+        onChange={handleChange}
       />
       <Input
         titlemarginBottom={"10px"}
         title={"전화번호"}
         marginBottom={"30px"}
         name={"JoinPhoneInput"}
-        placeholder={"전화번호를 입력해주세요"}
+        placeholder={"전화번호를 입력해주세요 (010-0000-0000)"}
+        onChange={handleChange}
       />
       <style.Label>주소</style.Label>
       <style.PlaceInput>
-        <select onChange={handleSelectColor} style={{ color: selectColor }}>
+        <select
+          onChange={handleSelectColor}
+          style={{ color: selectColor }}
+          name={"JoinPlaceInput"}
+        >
           <option value="text" selected>
             주소 선택하기
           </option>
@@ -86,7 +169,8 @@ function Join() {
         title={"아이디"}
         marginBottom={"30px"}
         name={"JoinIdInput"}
-        placeholder={"아이디를 입력해주세요"}
+        placeholder={"아이디를 입력해주세요 (영어와 숫자만 가능)"}
+        onChange={handleChange}
       />
       <Input
         titlemarginBottom={"10px"}
@@ -94,14 +178,17 @@ function Join() {
         marginBottom={"30px"}
         name={"JoinPasswordInput"}
         type={"password"}
-        placeholder={"비밀번호를 입력해주세요"}
+        placeholder={"비밀번호를 입력해주세요 (영어, 숫자를 포함해서 8-10글자)"}
+        onChange={handleChange}
       />
       <Input
         titlemarginBottom={"10px"}
         title={"비밀번호 확인"}
         marginBottom={"30px"}
         name={"JoinPasswordCheckInput"}
+        type={"password"}
         placeholder={"비밀번호를 한 번 더 입력해주세요"}
+        onChange={handleChange}
       />
       <style.OuterButton>
         <FullButton
@@ -109,6 +196,7 @@ function Join() {
           width={"400px"}
           height={"25px"}
           margin={"10px 0px"}
+          onClick={handleSubmit}
         />
       </style.OuterButton>
       <style.OuterButton>
