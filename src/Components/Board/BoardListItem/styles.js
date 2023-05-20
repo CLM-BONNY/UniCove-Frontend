b4653094366c7bb34117864802b881cb4d6c86ea
김민선 <alstjs4739@kookmin.ac.kr>
2023-05-20 19:16:26 +0900

    feat: 게시판 글 컴포넌트 구현

---
+++ src/Components/Board/BoardListItem/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Wrap = styled.div`
+  padding: 20px 15px;
+`;
+
+export const Title = styled.div`
+  margin-bottom: 5px;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+export const Content = styled.div`
+  margin-bottom: 10px;
+  font-size: 14px;
+`;
+
+export const IconBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  color: #84ccd4;
+  &:last-child {
+    color: #0f4c75;
+  }
+`;
+
+export const BottomBlock = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 20px;
+`;
