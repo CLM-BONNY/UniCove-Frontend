f07190d9b1d6a238e4f6fbd67d54e109c6ebf55e
김민선 <alstjs4739@kookmin.ac.kr>
2023-05-22 21:26:19 +0900

    feat: 검색 컴포넌트 지도 페이지 부분 삭제

--- src/Components/SearchBox/styles.js
+++ src/Components/SearchBox/styles.js
@@ -3,17 +3,16 @@ import styled from "styled-components";
 export const Wrap = styled.div`
   margin: ${(props) => props.margin};
   padding: 7px;
-  width: ${(props) => props.title === "지도" && "70%"};
-  height: 7vh;
+  height: 5vh;
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 18%;
-  border-radius: ${(props) => (props.title === "지도" ? "10px" : "0px")};
-  border: ${(props) => (props.title === "지도" ? "1px solid #e0e0e0" : "none")};
   border-bottom: 1px solid #e0e0e0;
+  background-color: #ffffff;
   color: #808080;
   font-weight: 600;
+  z-index: 100;
   > img {
     width: 20px;
     height: 20px;
