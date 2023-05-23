7f26f1fc09db2ccc20f928f60abef46aaa57a67c
didskfo <didskfo2003@gmail.com>
2023-05-23 14:28:14 +0900

    chore. 간격 조정

--- src/Pages/MyPage/styles.js
+++ src/Pages/MyPage/styles.js
@@ -2,12 +2,12 @@ import styled, { css } from "styled-components";
 
 export const Wrap = styled.div`
   padding: 0px 20px 0;
-  height: 100%;
+  height: 90vh;
 `;
 
 export const ProfileImg = styled.div`
   width: 220px;
   top: 93px;
   left: 128px;
-  margin: 20px auto 100px auto;
+  margin: 20px auto 80px auto;
 `;
