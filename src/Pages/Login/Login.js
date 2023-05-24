cb65f216a87c6b0c079aac7a86772dadb769830b
김민선 <alstjs4739@kookmin.ac.kr>
2023-05-25 02:37:08 +0900

    chore: 로그인 페이지 스타일 수정

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -50,56 +50,52 @@ function Login() {
   };
   const navigate = useNavigate();
   return (
-    <>
-      <style.Wrap>
-        <style.LogoBlock>
-          <img
-            className="logoImg"
-            alt="로고 이미지"
-            src={process.env.PUBLIC_URL + "Images/Login/UniCove.svg"}
-          ></img>
-        </style.LogoBlock>
-        <form>
-          <Input
-            titlemarginBottom={"10px"}
-            title={"아이디"}
-            marginBottom={"30px"}
-            name={"LoginIdInput"}
-            placeholder={"아이디를 입력해주세요"}
-            onChange={handleChange}
-          />
-          <Input
-            titlemarginBottom={"10px"}
-            title={"비밀번호"}
-            marginBottom={"30px"}
-            name={"LoginPasswordInput"}
-            type={"password"}
-            placeholder={"비밀번호를 입력해주세요"}
-            onChange={handleChange}
-          />
-        </form>
-        <style.OuterButton>
-          <FullButton
-            btnName={"로그인"}
-            width={"400px"}
-            height={"25px"}
-            margin={"10px 0px"}
-            onClick={handleSubmit}
-          />
-        </style.OuterButton>
-        <style.OuterButton>
-          <StrokeButton
-            btnName={"회원가입"}
-            width={"400px"}
-            height={"25px"}
-            margin={"0px 0px 20px 0px"}
-            onClick={() => {
-              navigate("../join");
-            }}
-          />
-        </style.OuterButton>
-      </style.Wrap>
-    </>
+    <style.Wrap>
+      <style.LogoBlock>
+        <img
+          className="logoImg"
+          alt="로고 이미지"
+          src={process.env.PUBLIC_URL + "Images/Login/UniCove.svg"}
+        />
+      </style.LogoBlock>
+      <form>
+        <Input
+          titlemarginBottom={"10px"}
+          title={"아이디"}
+          marginBottom={"30px"}
+          name={"LoginIdInput"}
+          placeholder={"아이디를 입력해 주세요"}
+          onChange={handleChange}
+        />
+        <Input
+          titlemarginBottom={"10px"}
+          title={"비밀번호"}
+          marginBottom={"30px"}
+          name={"LoginPasswordInput"}
+          type={"password"}
+          placeholder={"비밀번호를 입력해 주세요"}
+          onChange={handleChange}
+        />
+      </form>
+      <style.OuterButton>
+        <FullButton
+          btnName={"로그인"}
+          width={"95%"}
+          height={"25px"}
+          margin={"10px 0px"}
+          onClick={handleSubmit}
+        />
+        <StrokeButton
+          btnName={"회원가입"}
+          width={"95%"}
+          height={"25px"}
+          margin={"0px 0px 20px 0px"}
+          onClick={() => {
+            navigate("../join");
+          }}
+        />
+      </style.OuterButton>
+    </style.Wrap>
   );
 }
 
