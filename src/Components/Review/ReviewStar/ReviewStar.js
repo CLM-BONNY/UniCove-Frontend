06fa412ead7d98accb893ad3086c6c835bb90c63
김민선 <alstjs4739@kookmin.ac.kr>
2023-05-26 22:43:21 +0900

    feat: 리뷰 별점 컴포넌트 별점 함수 구현

--- src/Components/Review/ReviewStar/ReviewStar.js
+++ src/Components/Review/ReviewStar/ReviewStar.js
@@ -1,31 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import * as style from "./styles";
 
 function ReviewStar(props) {
+  const [clickedStarIndex, setClickedStarIndex] = useState(0);
+
+  const handleStarClick = (num) => {
+    setClickedStarIndex(num);
+    props.onClick?.();
+    console.log(num);
+  };
+
+  const getStarIcon = (num) => {
+    if (clickedStarIndex >= num) {
+      return process.env.PUBLIC_URL + "/Images/Review/FullStarIcon.svg";
+    }
+    return process.env.PUBLIC_URL + "/Images/Review/StrokeStarIcon.svg";
+  };
+
   return (
     <style.Wrap margin={props.margin} gap={props.gap} imgWidth={props.imgWidth}>
-      <img
-        src={process.env.PUBLIC_URL + "/Images/Review/StrokeStarIcon.svg"}
-        onClick={props.onClick}
-      />
-      <img
-        src={process.env.PUBLIC_URL + "/Images/Review/StrokeStarIcon.svg"}
-        onClick={props.onClick}
-      />
-      <img
-        src={process.env.PUBLIC_URL + "/Images/Review/StrokeStarIcon.svg"}
-        onClick={props.onClick}
-      />
-      <img
-        src={process.env.PUBLIC_URL + "/Images/Review/StrokeStarIcon.svg"}
-        onClick={props.onClick}
-      />
-      <img
-        src={process.env.PUBLIC_URL + "/Images/Review/StrokeStarIcon.svg"}
-        onClick={props.onClick}
-      />
+      {[1, 2, 3, 4, 5].map((num) => (
+        <img
+          key={num}
+          src={getStarIcon(num)}
+          onClick={() => handleStarClick(num)}
+        />
+      ))}
     </style.Wrap>
   );
 }
 
 export default ReviewStar;
+
