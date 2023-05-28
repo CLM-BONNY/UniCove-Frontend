df8af6faecf6e122493d3c6c02b5b85d72b3e1c0
김민선 <alstjs4739@kookmin.ac.kr>
2023-05-28 20:37:39 +0900

    feat. 모달 컴포넌트 구현

---
+++ src/Components/Modal/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import Modal from "styled-react-modal";
+
+export const TypeModal = Modal.styled`
+  background-color: none;
+`;
+
+export const IconBlock = styled.div`
+  display: flex;
+  align-items: center;
+  &:last-child {
+    right: 20px;
+    float: right;
+    position: absolute;
+  }
+`;
+
+export const ButtonBlock = styled.div`
+  position: absolute;
+  bottom: 7%;
+  left: 50%;
+  transform: translate(-50%, 0%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+`;
